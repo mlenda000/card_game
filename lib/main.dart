3602c8bf850866d49717ffcd8e25f504042d7f8d
mlenda000 <138724906+mlenda000@users.noreply.github.com>
2024-10-09 15:11:33 -0400

    Update main.dart
    updated to support certificate creation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:card_game/providers/game_provider.dart';
+import 'dart:io';
 import 'package:card_game/providers/crazy_eights_game_provider.dart';
 import 'package:card_game/providers/thirty_one_game_provider.dart';
 import 'package:card_game/screens/game_screen.dart';
@@ -8,7 +8,17 @@ import 'package:provider/provider.dart';
 final navigatorKey = GlobalKey<NavigatorState>();
 final rootScaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
 
+//added to allow for self-signed certificates for the API import is dart:io
+ class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => CrazyEightsGameProvider()),
     ChangeNotifierProvider(create: (_) => ThirtyOneGameProvider()),
